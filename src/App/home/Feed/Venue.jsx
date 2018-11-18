6ae6976c2a91c0facce8b632a8f4a92968f24013
Mantas Gelezinis <Mantsvas@gmail.com>
2018-11-18 23:31:20 +0200

    Default Venue/Avatar images

--- src/App/home/Feed/Venue.jsx
+++ src/App/home/Feed/Venue.jsx
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 class Venue extends Component {
     render() {
         const { venue } = this.props;
+        console.log(venue);
         return (
             <div className="card">
-                <img className="card-img-top" src="..." alt="Image" />
+                <img className="card-img-top" src={"/images/venues/" + venue.venue_photo} alt="Image" />
                 <div className="card-body">
                     <h5 className="card-title">{venue.name}</h5>
                 </div>
