9e25635ce4ce24fc8978bad1cee76259e3873244
Pijus Pauliukonis <pijus.pauliukonis@gmail.com>
2018-11-29 16:53:06 +0200

    sport venues judged by sport type.

--- src/App/home/Event/EventCreateForm.jsx
+++ src/App/home/Event/EventCreateForm.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 import axios from 'axios';
-import { fetchVenues, fetchSports } from '../../../../assets/js/fetchPublic';
+import { fetchSports } from '../../../../assets/js/fetchPublic';
 
 class EventCreateForm extends Component {
     state = {
@@ -15,14 +15,11 @@ class EventCreateForm extends Component {
             sportVenue: '',
         },
         message: '',
-        venues: [],
         sports: [],
+        venues: [],
     };
 
     async componentDidMount() {
-        await fetchVenues()
-            .then((response) => this.setState({ venues: response.data }))
-            .catch((errors) => console.log(errors));
         await fetchSports()
             .then((response) => this.setState({ sports: response.data }))
             .catch((errors) => console.log(errors));
@@ -30,9 +27,19 @@ class EventCreateForm extends Component {
 
     handleChange = (event) => {
         const { name, value } = event.target;
+        const { event: { sportType } } = this.state;
         this.setState(
             { event: { ...this.state.event, [name]: value } },
         );
+        if (name === 'sportType'){
+            this.filterSportVenues(value);
+        }
+    };
+
+    filterSportVenues = (sportType) => {
+        const {sports} = this.state;
+        const venues = sports.filter(sport => sport.id == sportType)[0].sport_venues;
+        this.setState({venues});
     };
 
     handleSubmit = (event) => {
@@ -68,8 +75,7 @@ class EventCreateForm extends Component {
                     </div>
                     {
                         message
-                        ? message
-                        : null
+                        && message
                     }
                     <div className="card-body">
                         <form onSubmit={this.handleSubmit}>
@@ -97,18 +103,20 @@ class EventCreateForm extends Component {
                                     }
                                 </select>
                             </div>
-                            <div className="form-group">
+                            { venues.length > 0
+                            && <div className="form-group">
                                 <label>Venue</label>
                                 <select className="form-control" name="sportVenue"
                                         onChange={() => this.handleChange(event)}>
                                     <option value=""></option>
                                     {
-                                        venues.map((venue) => (
+                                        venues.map(venue => (
                                             <option key={venue.id} value={venue.id}>{venue.name}</option>
                                         ))
                                     }
                                 </select>
                             </div>
+                            }
                             <div className="form-group">
                                 <FormControl margin="normal" fullWidth={true}>
                                     <TextField
