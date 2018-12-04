40162268b4ab6bd6901f74ab48773c26027afef8
Pijus Pauliukonis <pijus.pauliukonis@gmail.com>
2018-12-04 11:33:12 +0200

    EventApplicationController

---
+++ src/Controller/EventApplicationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerBuilder;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventApplicationController extends AbstractController
+{
+    /**
+     * @Route("/api/user/applications", name="user_application")
+     */
+    public function getApplicationByUser()
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $serializer = SerializerBuilder::create()->build();
+        $applications = json_decode(
+            $serializer->serialize($user->getUserApplications(), 'json', SerializationContext::create()->setGroups(array('user')))
+        );
+
+        return new JsonResponse($applications, JsonResponse::HTTP_OK);
+    }
+}
